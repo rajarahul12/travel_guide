62dbf5c3f843c6141b0b9afd7c83188b4507d528
Raja <maddula.rajarahul@gmail.com>
2018-09-01 19:05:20 +0530

    Changing Animations

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -4,7 +4,7 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 export class MapContainer extends Component {
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
+      <Map google={this.props.google} zoom={14} className="anim">
         <Marker onClick={this.onMarkerClick} name={"Current location"} />
 
         <InfoWindow onClose={this.onInfoWindowClose}>
